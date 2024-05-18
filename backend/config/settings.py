4c3fb1ba24e661d049d7efe5422d17547ffcb83e
Badmajor <v.balonkin@gmail.com>
2024-05-18 12:09:55 +0300

    flake8

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -132,7 +132,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.TokenAuthentication",
     ],
-git     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
 }
 
